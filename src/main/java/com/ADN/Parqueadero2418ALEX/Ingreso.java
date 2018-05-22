86025eea06bc41b4f0e22d2ad6bd010881ed3fde
Alexander Angulo Uribe - Ceiba Software <alexander.angulo@ceiba.com.co>
2018-05-22 15:53:42 -0500

    add archive

---
+++ src/main/java/com/ADN/Parqueadero2418ALEX/Ingreso.java
@@ -0,0 +1,14 @@
+package com.ADN.Parqueadero2418ALEX;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.time.LocalDate;
+
+@SpringBootApplication
+public class Ingreso {
+	
+	private int TipoVehiculo;
+	private Date FechaIngre;
+	
+	
+
+}
---
+++ src/main/java/com/ADN/Parqueadero2418ALEX/Parqueadero2418AlexApplication.java
@@ -0,0 +1,12 @@
+package com.ADN.Parqueadero2418ALEX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Parqueadero2418AlexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Parqueadero2418AlexApplication.class, args);
+	}
+}
